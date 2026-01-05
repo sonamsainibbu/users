77811094d5d876f285acc136a03055e377093bbc
Sonam <sonamsainibbu7@gmail.com>
2026-01-05 08:29:25 +0530

    add two file entity and dto for user

---
+++ src/main/java/com/user/dtos/UserDto.java
@@ -0,0 +1,31 @@
+package com.user.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 30, message = "Username must be between 3 and 30 characters")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Please provide a valid email address")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 12, message = "password should be 6 to 12 character")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+    )
+    private String password;
+}
---
+++ src/main/java/com/user/entities/User.java
@@ -0,0 +1,32 @@
+package com.user.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username",
+            unique = true,
+            nullable = false,
+            length = 30)
+    private String username;
+
+    @Column(name = "email",
+            unique = true,
+            nullable = false)
+    private String email;
+
+    @Column(name = "password",
+            nullable = false)
+    private String password;
+}
